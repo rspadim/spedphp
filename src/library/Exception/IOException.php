8b7a5d059c0046e302a7c2aeb58469640db147ca
Roberto L. Machado <linux.rlm@gmail.com>
2013-05-05 10:24:15 -0300

    Inclusões spl excptions

---
+++ src/library/Exception/IOException.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ * NFePHP (http://www.nfephp.org/)
+ *
+ * @link      http://github.com/nfephp-org/nfephp for the canonical source repository
+ * @copyright Copyright (c) 2008-2013 NFePHP (http://www.nfephp.org)
+ * @license   http://www.gnu.org/licenses/lesser.html LGPL v3
+ * @license   http://www.gnu.org/licenses/gpl.html GNU/GPL v.3
+ * @package   NFePHP
+ */
+
+namespace libray\Exception;
+
+/**
+ * Exception class for NFePHP
+ *
+ * @category   NFePHP
+ * @package    NFePHP
+ * @subpackage NFePHP_Exception
+ */
+class IOException extends RuntimeException
+{
+}
